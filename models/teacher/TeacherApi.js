6399c38b6124f7908fcc489c3196f8e70a68b8af
shreyasbarve <shreyasbarve13@gmail.com>
2020-11-15 17:24:02 +0530

    minor fixes

--- models/teacher/TeacherApi.js
+++ models/teacher/TeacherApi.js
@@ -6,56 +6,62 @@ const TEACHER_URL = "https://learnzilla.herokuapp.com/teacher";
 const TeacherApi = {
   // Login and Register
   register: async (payload) => {
-    return await axios.post(`${TEACHER_URL}/register`, payload)
+    return await axios.post(`${TEACHER_URL}/register`, payload);
   },
   login: async (payload) => {
-    return await axios.post(`${TEACHER_URL}/login`,payload)
+    return await axios.post(`${TEACHER_URL}/login`, payload);
   },
   getTeacherDetails: async (payload) => {
-    return await axios.post(`${TEACHER_URL/}`, payload)
+    return await axios.post(`${TEACHER_URL}/`, payload);
   },
 
   // HomePage
   getClasses: async (payload) => {
-    return await axios.post(`${CLASS_URL}/`, payload[0])
+    return await axios.post(`${CLASS_URL}/`, payload[0]);
   },
   createClass: async (payload) => {
-    return await axios.post(`${CLASS_URL}/create/`, payload)
+    return await axios.post(`${CLASS_URL}/create/`, payload);
   },
 
   // Inside Class
   getAssignments: async (classId) => {
-    return await axios.get(`${CLASS_URL}/viewassignments/${classId}/`)
+    return await axios.get(`${CLASS_URL}/viewassignments/${classId}/`);
   },
   createAssignment: async (payload) => {
-    return await axios.get(`${CLASS_URL}/create-new-assign/`, payload)
+    return await axios.get(`${CLASS_URL}/create-new-assign/`, payload);
   },
 
   getMarks: async (classId) => {
-    return await axios.get(`${CLASS_URL}/viewmarks/classroomid/${classId}/`)
+    return await axios.get(`${CLASS_URL}/viewmarks/classroomid/${classId}/`);
   },
   addMarks: async (payload) => {
-    return await axios.post(`${CLASS_URL}/addmarks/`, payload)
+    return await axios.post(`${CLASS_URL}/addmarks/`, payload);
   },
 
   getStudents: async (classId, payload) => {
-    return await axios.post(`${CLASS_URL}/viewstudents/${classId}/`, payload[0])
+    return await axios.post(
+      `${CLASS_URL}/viewstudents/${classId}/`,
+      payload[0]
+    );
   },
   addStudent: async (payload) => {
-    return await axios.post(`${CLASS_URL}/addstudent/`, payload)
+    return await axios.post(`${CLASS_URL}/addstudent/`, payload);
   },
 
   getAttendance: async (classId) => {
-    return await axios.get(`${CLASS_URL}/viewattendance/${classId}`)
+    return await axios.get(`${CLASS_URL}/viewattendance/${classId}`);
   },
   addAttendance: async (payload) => {
-    return await axios.post(`${CLASS_URL}/markattendance/`, payload)
+    return await axios.post(`${CLASS_URL}/markattendance/`, payload);
   },
 
   // Teacher Profile
   changePassword: async (payload, teacherName) => {
-    return await axios.post(`${TEACHER_URL}/${teacherName}/change-password`, payload)
-  }
+    return await axios.post(
+      `${TEACHER_URL}/${teacherName}/change-password`,
+      payload
+    );
+  },
 };
 
 export default TeacherApi;
